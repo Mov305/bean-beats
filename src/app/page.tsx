bdb59af10e5669c204a828c2693cb94bbfc31f2e
Abdelrhman Samy Saad <abdelrhman.samy.saad@gmail.com>
2024-07-24 12:06:17 +0300

    blob upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { SignIn } from '@/components/auth/sign-in'
+import UploadImage from '@/components/upload/upload-button'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Next.js Auth</h1>
+      <UploadImage />
       <SignIn />
     </main>
   )
