2c554f78609e7c82f5828172b565abbd00bf0ea0
Abdelrhman Samy Saad <abdelrhman.samy.saad@gmail.com>
2024-07-24 12:07:15 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import UploadImage from '@/components/upload/upload-button'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Next.js Auth</h1>
+      <h1 className="text-4xl font-bold">Next.js</h1>
       <UploadImage />
       <SignIn />
     </main>
